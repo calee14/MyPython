80e94a367bdd1db12ae64b41eb8a5187d268a523
calee14 <caplee1000@gmail.com>
2017-12-04 20:12:25 -0800

    organized data in json

--- webscrapeworkspace/bureauwebscrape/test.py
+++ webscrapeworkspace/bureauwebscrape/test.py
@@ -61,11 +61,60 @@ for row in contents:
 	else:
 		# if try is succuessful
 		for header in header_list:
+			append_array = []
 			for num in header.requests_objects:
-				header.children.append(str(children[num].text))
+				append_array.append(str(children[num].text))
+			header.addChild(append_array)
 
+def init_list_of_objects(size):
+    list_of_objects = list()
+    for i in range(0,size):
+        list_of_objects.append( list() ) #different object reference each time
+    return list_of_objects
+
+arrays = init_list_of_objects(23) #[None] * len(header_list[0].children)  # Create list of 100 'None's
+print len(arrays) 
+# check if we have the same amount
+for header in header_list:
+	try:
+		for header2 in header_list:
+			try:
+				length_of_all_arrays = len(header.children)
+				len(header.children) == len(header2.children)
+				print("okay")
+			except Exception as e:
+				print e
+	except Exception as e:
+		print e
+	else:
+		for child in header.children:
+			child_index = header.children.index(child)
+			print child_index
+			arrays[child_index].append(child)
+
+print arrays[0]
+json_occupations_data = []
+for array in arrays:
+	json_array = []
+	for header in header_list:
+		json_data = {
+			header.title : array[header_list.index(header)]
+		}
+		json_array.append(json_data)
+	json_occupations_data.append(json_array)
+
+# write it in json file
+filename = "careers.json"
+f = open(filename, "w")
+jsonstuff = json.dumps(json_occupations_data, indent=4)
+f.write(jsonstuff)
+# for header in header_list:
+# 	for index in len(header.children):
+# 		pass
 filename = "test.txt"
 f = open(filename, "w")
 for header in header_list:
 	for child in header.children:
-		f.write(''+header.title+' '+child+'\n')
+		f.write(''+header.title+' '+str(child)+'\n')
+
+	f.write(''+str(len(header.children))+' \n')
