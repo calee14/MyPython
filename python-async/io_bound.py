1dbe50738b875c5116538fe140eb4e4d276da478
calee14 <caplee1000@gmail.com>
2019-08-13 22:52:09 -0700

    adding the the first io bound example

---
+++ python-async/io_bound.py
@@ -0,0 +1,16 @@
+import time
+
+def two():
+	print('starting two')
+	time.sleep(2)
+	print('hey two')
+
+def four():
+	print('starting four')
+	time.sleep(4)
+	print('hey four')
+
+start = time.time()
+two()
+four()
+print(f'{time.time()-start}')
