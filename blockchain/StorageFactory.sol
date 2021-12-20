b9c2399c751e849d390f13fc4278d3588181bff0
calee14 <caplee1000@gmail.com>
2021-12-20 14:20:43 -0800

    complete code for using other contracts

---
+++ blockchain/StorageFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License_Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        // create a new contract obj
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Addresss = address of the contract
+        // ABI = Application Binary Interface
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStorage.retrieve();
+    }
+}
