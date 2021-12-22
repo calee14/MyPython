6ed2ed1c36600d36f351229440fc9ebc3c0ddfff
calee14 <caplee1000@gmail.com>
2021-12-22 12:48:42 -0800

    deploy a contract in brownie

--- blockchain/brownie_simp_stor/scripts/deploy.py
+++ blockchain/brownie_simp_stor/scripts/deploy.py
@@ -1,13 +1,26 @@
-from brownie import network, accounts, config
+from brownie import network, accounts, config, SimpleStorage
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-def deploy_contract():
-    account = accounts.add(os.getenv("PRIVATE_KEY"))
-    print(account)
+def deploy_simple_storage():
+    account = accounts.add(config['wallets']['from_key'])
+    # this deploy function will create, sign and send the transaction
+    # it will also know if the transaction is a call or a transaction
+    simple_storage = SimpleStorage.deploy({"from": account})
+
+    # brownie knows that this retreive is a call 
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    
+    # simple storage make a transaction to store signing with our account
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 def main():
-    deploy_contract()
+    deploy_simple_storage()
     print('hello')
