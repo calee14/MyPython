70a470428fce796a7089d16793ee3460ac866c69
calee14 <caplee1000@gmail.com>
2021-12-24 16:15:25 -0800

    return contract fund me

--- blockchain/brownie_fund_me/scripts/deploy.py
+++ blockchain/brownie_fund_me/scripts/deploy.py
@@ -1,15 +1,16 @@
 from brownie import FundMe, config, network, MockV3Aggregator
-from brownie_fund_me.scripts.utils import deploy_mocks
+from scripts.utils import deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.utils import get_account
 
 def deploy_fund_me():
     account = get_account()
-    # if we're on rinkeby use the rinkeby aggregator address oracle
-    if network.show_active() != 'development':
+    # if we're on rinkeby or local blockchain use the rinkeby aggregator address oracle
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config['networks'][network.show_active()][
             'eth_usd_price_feed'
         ]
     else:
+        # deploy mock aggregator to the network
         print(f'The active network is {network.show_active()}')
         print('deploying mocks')
         deploy_mocks()
@@ -23,6 +24,8 @@ def deploy_fund_me():
         publish_source=config['networks'][network.show_active()]['verify']
     )
     print(f'Contract deployed to {fund_me.address}')
+
+    return fund_me
     
 def main():
     deploy_fund_me()
