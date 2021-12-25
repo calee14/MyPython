2aa043f8bb226dbb818c1859958524202234e708
calee14 <caplee1000@gmail.com>
2021-12-24 16:15:38 -0800

    fund and withdraw to the contract

---
+++ blockchain/brownie_fund_me/scripts/fund_and_withdraw.py
@@ -0,0 +1,19 @@
+from brownie import FundMe, config, accounts, network
+from scripts.utils import get_account
+
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
+    entrance_fee = fund_me.getEntranceFee()
+    print('The entrance fee is ', entrance_fee)
+    print('Funding')
+    fund_me.fund({'from': account, 'value': entrance_fee})
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({'from': account})
+
+def main():
+    fund()
+    withdraw()
