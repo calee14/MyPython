d051e226a9c48f28cfa4a8a0585cbce1db4b00df
calee14 <caplee1000@gmail.com>
2021-12-31 00:23:49 -0800

    trying to deploy the lottery

--- blockchain/sc_lottery/scripts/deploy.py
+++ blockchain/sc_lottery/scripts/deploy.py
@@ -5,7 +5,10 @@ def deploy_lottery():
     account = get_account()
 
     lottery = Lottery.deploy(
-        get_contract('eth_usd_price_feed')
+        get_contract('eth_usd_price_feed').address,
+        get_contract('vrf_coordinator').address,
+        get_contract('link_token').address,
+        
     )
 
 def main():
