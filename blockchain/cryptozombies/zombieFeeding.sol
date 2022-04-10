fc6dc97f44cb50dcc846d054c6138921fd17d583
calee14 <caplee1000@gmail.com>
2022-04-10 12:21:45 -0700

    func modifier usage

--- blockchain/cryptozombies/zombieFeeding.sol
+++ blockchain/cryptozombies/zombieFeeding.sol
@@ -28,7 +28,9 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
 
-    // Initialize kittyContract here using `ckAddress` from above
+	function setKittyContractAddress(address _address) external onlyOwner {
+		kittyContract = KittyInterface(_address);
+	}
 
     function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
